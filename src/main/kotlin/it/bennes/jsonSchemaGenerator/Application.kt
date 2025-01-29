4a902fa57fa69e7eadba9b136cf630fd2ca6f54d
bennesp <bennesp@users.noreply.github.com>
2025-01-29 10:47:20 +0100

    update to ktor v3

--- src/main/kotlin/it/bennes/jsonSchemaGenerator/Application.kt
+++ src/main/kotlin/it/bennes/jsonSchemaGenerator/Application.kt
@@ -4,7 +4,7 @@ import it.bennes.jsonSchemaGenerator.exceptions.HTTPException
 import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.plugins.callid.*
-import io.ktor.server.plugins.callloging.*
+import io.ktor.server.plugins.calllogging.*
 import io.ktor.server.plugins.compression.*
 import io.ktor.server.plugins.statuspages.*
 import io.ktor.server.response.*
